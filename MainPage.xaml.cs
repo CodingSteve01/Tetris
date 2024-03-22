a97305e3a61f29c107c14fd0ea9904419d404ca0
Steffen Lüling <steffen.lueling@gmail.com>
2024-03-22 10:38:15 +0100

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace Tetris;
+
+public partial class MainPage : ContentPage
+{
+    private Timer _timer;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnMoveLeft(object sender, EventArgs e)
+    {
+    }
+
+    private void OnMoveRight(object sender, EventArgs e)
+    {
+    }
+
+    private void OnMoveDown(object sender, EventArgs e)
+    {
+    }
+
+    private void OnFlip(object sender, EventArgs e)
+    {
+    }
+}
